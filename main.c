315782e7fe0219a1c4269798d31cd178af836d9a
y.zouroufchi <y.zouroufchi@mohaymen.co>
2020-08-26 10:24:15 +0430

    Initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+//Mathematical operators
+/*
+    int x = 9;
+    int y = 4;
+    int z;
+    double xDouble = 9.0;
+    double yDouble = 4.0;
+    double z2 = xDouble / y;
+    z = x / y;
+    int r = x % y;
+    //x = y * z + r
+    printf("%d %f %d\n", z, z2, r);
+*/
+//getting input via scanf
+/*
+    int input, c;
+    scanf("%d Enter: %d", &input, &c);
+    printf("%d:%f", c, input * 2.5);
+*/
+//if/else
+/*
+    int p = 4;
+    //a % b = 0 -> b | a
+    if (p % 3 == 0){
+        printf("3 | p");
+    } else {
+        printf("not 3 | p");
+    }
+*/
+    return 0;
+}
